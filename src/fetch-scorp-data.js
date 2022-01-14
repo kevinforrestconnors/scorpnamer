4a95a60e78eea6d314730a4fd7b855648a1d915c
Kevin Forrest Connors <kevinforrestconnors@gmail.com>
2022-01-13 23:14:14 -0500

    fixes broken build

--- src/fetch-scorp-data.js
+++ src/fetch-scorp-data.js
@@ -1,16 +1,14 @@
-import cheerio from "cheerio";
+//import cheerio from "cheerio";
 
 export async function getScropInfo(scorpId) {
   const url = `https://crossorigin.me/https://radstrike.com/scorpions/info/?number=${scorpId}`;
 
   try {
-    const response = await fetch(url, { mode: "no-cors" })
-      .then((d) => d.text())
-      .then((d) => console.log(d));
+    const response = await fetch(url);
     const body = await response.text();
 
-    const $ = cheerio.load(body);
-    console.log(response);
+    //const $ = cheerio.load(body);
+    console.log(body);
   } catch (err) {
     console.error(err);
   }
